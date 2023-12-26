f722719ea0e8afc6e78735011def083f6a40f1c1
Ivan Kharitonovich <ikh@trackman.com>
2023-12-26 22:05:13 +0300

    Add MenuOrder

--- Editor/Scripts/Core/RecompileUtils.cs
+++ Editor/Scripts/Core/RecompileUtils.cs
@@ -6,9 +6,9 @@ namespace Trackman.Editor.Core
     public static class RecompileUtils
     {
         #region Methods
-        [MenuItem("Trackman/Recompile scripts %r")]
+        [MenuItem("Trackman/Recompile scripts %r", false, (int)MenuOrder.Functions)]
         public static void RecompileScripts() => CompilationPipeline.RequestScriptCompilation();
-        [MenuItem("Trackman/Recompile all scripts %&r")]
+        [MenuItem("Trackman/Recompile all scripts %&r", false, (int)MenuOrder.Functions + 1)]
         public static void RecompileAllScripts() => CompilationPipeline.RequestScriptCompilation(RequestScriptCompilationOptions.CleanBuildCache);
         #endregion
     }
---
+++ Editor/Scripts/Interface/Enums.Core.cs
@@ -0,0 +1,10 @@
+
+namespace Trackman.Editor
+{
+    public enum MenuOrder
+    {
+        Build = 100,
+        Tools = 300,
+        Functions = 400
+    }
+}
