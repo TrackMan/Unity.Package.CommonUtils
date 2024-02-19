37e81b8adec53dd73385838ca2eb0c1747c03012
Ivan Kharitonovich <ikh@trackman.com>
2024-02-19 11:20:11 +0300

    Add Timer

---
+++ Runtime/Scripts/Core/Timer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System;
+using System.Diagnostics;
+
+namespace Trackman
+{
+    [Serializable]
+    [DebuggerStepThrough]
+    public class Timer : ITimer
+    {
+        #region Fields
+        bool ITimer.started { get; set; }
+        bool ITimer.done { get; set; }
+        bool ITimer.pause { get; set; }
+
+        float ITimer.elapsed { get; set; } = -1;
+        float ITimer.acceleration { get; set; } = 1;
+        float ITimer.duration { get; set; } = -1;
+        bool ITimer.loop { get; set; }
+        #endregion
+
+        #region Properties
+        ITimer I => this.As<ITimer>();
+        public float Elapsed => I.elapsed;
+        public float Duration { get => I.duration; set => I.duration = value; }
+        public float Lerp => I.elapsed / I.duration;
+        public bool Started => I.started;
+        public bool Playing => I.started && !I.done;
+        public bool Pause { get => I.started && I.pause; set => I.pause = I.started && value; }
+        public bool Done => I.started && I.done;
+        public float Acceleration => I.acceleration;
+        public bool Loop => I.loop;
+        #endregion
+
+        #region Methods
+        public void Start(float duration) => Start(0, duration);
+        public void Start(float elapsed, float duration, float acceleration = 1.0f, bool loop = false) => I.StartDefault(elapsed, duration, acceleration, loop);
+        public void PreStart(float duration, float acceleration = 1.0f, bool loop = false) => Start(-Time.deltaTime, duration, acceleration, loop);
+        public void Cancel() => I.CancelDefault();
+        public void ChangeAcceleration(float acceleration) => I.ChangeAccelerationDefault(acceleration);
+        public bool ChronoTrigger() => I.ChronoTriggerDefault();
+        #endregion
+    }
+}
--- Runtime/Scripts/Interface/Interface.Core.cs
+++ Runtime/Scripts/Interface/Interface.Core.cs
@@ -60,4 +60,95 @@ namespace Trackman
         bool CanExecute => true;
         #endregion
     }
+
+
+    public interface ITimer
+    {
+        #region Properties
+        float Elapsed { get; }
+        float Duration { get; set; }
+        float Lerp { get; }
+        bool Started { get; }
+        bool Playing { get; }
+        bool Pause { get; set; }
+        bool Done { get; }
+        float Acceleration { get; }
+        bool Loop { get; }
+
+        #pragma warning disable S100, TM0006, InconsistentNaming
+        protected internal float elapsed { get; set; }
+        protected internal float duration { get; set; }
+        protected internal bool started { get; set; }
+        protected internal bool pause { get; set; }
+        protected internal bool done { get; set; }
+        protected internal float acceleration { get; set; }
+        protected internal bool loop { get; set; }
+        #pragma warning restore S100, TM0006, InconsistentNaming
+        #endregion
+
+        #region Methods
+        void Start(float duration) => Start(0, duration);
+        void Start(float elapsed, float duration, float acceleration = 1.0f, bool loop = false);
+        protected internal void StartDefault(float elapsed, float duration, float acceleration, bool loop)
+        {
+            started = true;
+            done = false;
+            pause = false;
+            this.elapsed = elapsed;
+            this.acceleration = acceleration;
+            this.duration = duration;
+            this.loop = loop;
+        }
+        void PreStart(float duration, float acceleration = 1.0f, bool loop = false) => Start(-Time.deltaTime, duration, acceleration, loop);
+        void Cancel();
+        protected internal void CancelDefault()
+        {
+            started = false;
+            done = false;
+            pause = false;
+            elapsed = -1;
+            acceleration = 1;
+            duration = -1;
+            loop = false;
+        }
+        void ChangeAcceleration(float acceleration);
+        protected internal void ChangeAccelerationDefault(float acceleration)
+        {
+            this.acceleration = acceleration;
+        }
+        /// <summary>
+        /// Moves timer by Time.deltaTime
+        /// </summary>
+        /// <returns>false if just finished, else true</returns>
+        bool ChronoTrigger();
+        protected internal bool ChronoTriggerDefault()
+        {
+            if (!Started) return !Done;
+            float dt = Time.deltaTime * Acceleration;
+            if (Pause) dt = 0;
+            if (Elapsed >= Duration)
+            {
+                if (Loop) elapsed = 0;
+                else done = true;
+            }
+            else if (Elapsed + dt > Duration)
+            {
+                if (Loop)
+                {
+                    elapsed = Elapsed + dt - Duration;
+                }
+                else
+                {
+                    elapsed = Duration;
+                    done = true;
+                }
+            }
+            else
+            {
+                elapsed += dt;
+            }
+            return !Done;
+        }
+        #endregion
+    }
 }
