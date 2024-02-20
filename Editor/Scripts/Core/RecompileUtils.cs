df989723221032b1e58353906d7dd42eb34cbd95
Dmitri Emeliov <dem@trackman.com>
2024-02-20 22:24:19 +0200

    Adds SyncSolution to RecompileUtils

--- Editor/Scripts/Core/RecompileUtils.cs
+++ Editor/Scripts/Core/RecompileUtils.cs
@@ -1,5 +1,8 @@
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEditor.Compilation;
+using System;
+using System.Reflection;
 
 namespace Trackman.Editor.Core
 {
@@ -10,6 +13,18 @@ namespace Trackman.Editor.Core
         public static void RecompileScripts() => CompilationPipeline.RequestScriptCompilation();
         [MenuItem("Trackman/Recompile all scripts %&r", false, (int)MenuOrder.Functions + 1)]
         public static void RecompileAllScripts() => CompilationPipeline.RequestScriptCompilation(RequestScriptCompilationOptions.CleanBuildCache);
+
+        [UsedImplicitly] public static void SyncSolution()
+        {
+            // enum ProjectGenerationFlag
+            // None = 0,
+            // Embedded = 1,
+            // Local = 2,
+            int oldValue = EditorPrefs.GetInt("unity_project_generation_flag", 3);
+            EditorPrefs.SetInt("unity_project_generation_flag", 3);
+            Type.GetType("Packages.Rider.Editor.RiderScriptEditor, Unity.Rider.Editor").GetMethod("SyncSolution", BindingFlags.Static | BindingFlags.Public).Invoke(null, Array.Empty<object>());
+            EditorPrefs.SetInt("unity_project_generation_flag", oldValue);
+        }
         #endregion
     }
 }
