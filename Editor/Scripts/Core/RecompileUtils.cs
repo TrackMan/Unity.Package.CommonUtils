806d5d5cd8771b4cc0f560cb471073145f9d759f
Eugene Mihailenco <emi@trackman.com>
2024-03-05 15:08:23 +0100

    Bugfix: Force exit on Unity application when SyncSolution done and adding logs

--- Editor/Scripts/Core/RecompileUtils.cs
+++ Editor/Scripts/Core/RecompileUtils.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEditor.Compilation;
 using System;
 using System.Reflection;
+using UnityEngine;
 
 namespace Trackman.Editor.Core
 {
@@ -16,14 +17,28 @@ namespace Trackman.Editor.Core
 
         [UsedImplicitly] public static void SyncSolution()
         {
+            Debug.Log($"[{nameof(RecompileUtils)}] Starting solution sync...");
+
             // enum ProjectGenerationFlag
             // None = 0,
             // Embedded = 1,
             // Local = 2,
             int oldValue = EditorPrefs.GetInt("unity_project_generation_flag", 3);
             EditorPrefs.SetInt("unity_project_generation_flag", 3);
-            Type.GetType("Packages.Rider.Editor.RiderScriptEditor, Unity.Rider.Editor").GetMethod("SyncSolution", BindingFlags.Static | BindingFlags.Public).Invoke(null, Array.Empty<object>());
-            EditorPrefs.SetInt("unity_project_generation_flag", oldValue);
+
+            try
+            {
+                Type.GetType("Packages.Rider.Editor.RiderScriptEditor, Unity.Rider.Editor").GetMethod("SyncSolution", BindingFlags.Static | BindingFlags.Public).Invoke(null, Array.Empty<object>());
+                Debug.Log($"[{nameof(RecompileUtils)}] Solution sync done");
+            }
+            catch (Exception exception)
+            {
+                Debug.LogException(exception);
+            }
+            finally{
+                EditorPrefs.SetInt("unity_project_generation_flag", oldValue);
+                EditorApplication.Exit(0);
+            }
         }
         #endregion
     }
