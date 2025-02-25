a85cb118399438d06a811c60702761865cba881e
Matt Krzaczek <mkrz@trackman.com>
2025-02-25 09:05:55 +0000

    Merged PR 83069: Nicer tests logging
    Focusing on the important stuff in tests logs

--- Runtime/Scripts/Interface/Core/CodeStatics.cs
+++ Runtime/Scripts/Interface/Core/CodeStatics.cs
@@ -8,6 +8,14 @@
         public static bool IncludeTests => false;
 #endif
 
+#if TESTS_LOGGING_VERBOSE
+        public static bool VerboseTestsLogging => true;
+#else
+        public static bool VerboseTestsLogging => false;
+#endif
+
         public static bool IsRunningTests { get; set; }
+
+        public static bool SkipVerboseLogs => IsRunningTests && !VerboseTestsLogging;
     }
 }
